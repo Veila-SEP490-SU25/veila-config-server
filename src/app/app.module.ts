fcdcff857531d937f91cdac8b67a0ea24de160ff
neru <ngocngocthuc@gmail.com>
2025-05-30 19:33:51 +0700

    Add TypeORM configuration and global config module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.model{.ts,.js}'],
+        synchronize: true,
+        charset: 'utf8mb4_unicode_ci',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
