e0e0b03aa2fdf6b72f45a6699dee181ab99c87f9
neru <ngocngocthuc@gmail.com>
2025-05-30 18:53:20 +0700

    Refactor main.ts to enhance server setup and add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,72 @@
-import { NestFactory } from '@nestjs/core';
+process.env.TZ = 'Asia/Ho_Chi_Minh'; // Set timezone to Vietnam
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+
+  app.enableCors({
+    origin: (process.env.FE_URL as string) || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+      'DNT',
+      'User-Agent',
+      'X-Requested-With',
+      'If-Modified-Since',
+      'Cache-Control',
+      'Content-Type',
+      'Range',
+      'Authorization',
+    ],
+    exposedHeaders: ['Content-Length', 'Content-Range', 'Content-Type'],
+    maxAge: 86400,
+  });
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Veila Configuration Server API')
+    .setDescription('API documentation for Veila Configuration Server')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('/swagger', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+      filter: true,
+      showExtensions: true,
+      showCommonExtensions: true,
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+
+  const host = process.env.LISTEN_HOST || '0.0.0.0';
+  const port = process.env.LISTEN_PORT || 3000;
+
+  await app.listen(port, host).then(() => {
+    Logger.log(`Server running on http://${host}:${port}`, 'Bootstrap');
+    Logger.log(
+      `Swagger running on http://${host}:${port}/swagger`,
+      'Bootstrap',
+    );
+  });
 }
 bootstrap();
