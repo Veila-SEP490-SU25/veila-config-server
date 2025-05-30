bb4b6afee8ebf14d9e2327bcc9a13d7d35627d7d
neru <ngocngocthuc@gmail.com>
2025-05-30 22:05:00 +0700

    Implement login functionality with error handling in AuthService and AuthController

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,9 +1,23 @@
-import { AuthService } from "@/app/auth/auth.service";
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { LoginRequest, LoginResponse } from '@/app/auth/auth.dto';
+import { AuthService } from '@/app/auth/auth.service';
+import { ItemResponse } from '@/common/models';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 @ApiTags('Auth Controller')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(
+    @Body() body: LoginRequest,
+  ): Promise<ItemResponse<LoginResponse>> {
+    const response = await this.authService.login(body);
+    return {
+      item: response,
+      message: 'Login successful',
+      statusCode: 200,
+    };
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TokenService } from '@/app/token';
 import { UserService } from '@/app/user';
+import { LoginRequest, LoginResponse } from '@/app/auth/auth.dto';
+import { PasswordService } from '@/app/password';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly tokenService: TokenService,
+    private readonly passwordService: PasswordService,
   ) {}
 
-
+  async login(body: LoginRequest): Promise<LoginResponse> {
+    const user = await this.userService.getByUsername(body.username);
+    if (!user) {
+      throw new NotFoundException('Tài khoản không tồn tại.');
+    }
+    const isPasswordValid = await this.passwordService.comparePassword(
+      body.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestException('Mật khẩu không chính xác.');
+    }
+    const accessToken = await this.tokenService.createToken(user);
+    const refreshToken = await this.tokenService.createToken(user, {
+      isRefresh: true,
+    });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
 }
